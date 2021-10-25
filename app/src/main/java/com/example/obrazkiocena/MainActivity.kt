3a6b7083da64b09edbb9feabd9c9311da1aea0ef
LeonCookie <LeonGdynia1105@gmail.com>
2021-10-25 15:23:54 +0200

    mapa test numero uno na widok

--- app/src/main/java/com/example/obrazkiocena/MainActivity.kt
+++ app/src/main/java/com/example/obrazkiocena/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.obrazkiocena
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,12 +17,23 @@ class MainActivity : AppCompatActivity() {
         lateinit var previous: Button
         lateinit var next: Button
         lateinit var delete: Button
+        lateinit var mapa: ImageView
         title="obrazy"
         widac = findViewById(R.id.checkBoxWidok)
         camera = findViewById(R.id.imageButtonCamera)
         previous =findViewById(R.id.imageButtonPrevious)
         next = findViewById(R.id.imageButtonNext)
         delete = findViewById(R.id.imageButtonDelete)
+        mapa = findViewById(R.id.imageViewMapa)
+        widac.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if (isChecked){
+                mapa.setVisibility(View.INVISIBLE);
+
+            }else{
+                mapa.setVisibility(View.VISIBLE);
+            }
+
+            }
     }
 
 
