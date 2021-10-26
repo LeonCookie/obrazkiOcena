cacdd038203b3911407765e7a9c2b7406d8899eb
LeonCookie <leongdynia1105@gmail.com>
2021-10-26 18:30:27 +0200

    zabawa z seekbar

--- app/src/main/java/com/example/obrazkiocena/MainActivity.kt
+++ app/src/main/java/com/example/obrazkiocena/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.obrazkiocena
 
-import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
-import android.media.Image
-import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.*
 
@@ -25,66 +22,98 @@ class MainActivity : AppCompatActivity() {
         lateinit var next: ImageButton
         lateinit var delete: ImageButton
         lateinit var mapa: ImageView
+        lateinit var Suwak: SeekBar
 
 
-        var  images = arrayListOf<Int>(R.drawable.shock,R.drawable.cringe,R.drawable.what,R.drawable.horny)
-        var i:Int = 0
+        var images =
+            arrayListOf<Int>(R.drawable.shock, R.drawable.cringe, R.drawable.what, R.drawable.horny)
+        var i: Int = 0
+        var r:Float
 
 
-        title="obrazy"
+
+
+        title = "obrazy"
         widac = findViewById<CheckBox>(R.id.checkBoxWidok)
         camera = findViewById<ImageButton>(R.id.imageButtonCamera)
-        previous =findViewById<ImageButton>(R.id.imageButtonPrevious)
+        previous = findViewById<ImageButton>(R.id.imageButtonPrevious)
         next = findViewById<ImageButton>(R.id.imageButtonNext)
         delete = findViewById<ImageButton>(R.id.imageButtonDelete)
         mapa = findViewById<ImageView>(R.id.imageViewObraek)
+        Suwak = findViewById<SeekBar>(R.id.seekBar2)
 
 
-        widac.setOnCheckedChangeListener{ buttonView, isChecked ->
-            if (isChecked){
+
+
+
+        widac.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
 
 
                 mapa.setVisibility(View.INVISIBLE);
 
-            }else{
+            } else {
                 mapa.setVisibility(View.VISIBLE);
             }
 
-            }
+        }
 
         next.setOnClickListener {
-            if(i<3){
+            if (i < 3) {
                 i++
                 mapa.setImageResource(images[i])
-            }else{
-                Toast.makeText(applicationContext, "ostatni obraz", Toast.LENGTH_SHORT ).show()
-                i=3
+            } else {
+                Toast.makeText(applicationContext, "ostatni obraz", Toast.LENGTH_SHORT).show()
+                i = 3
             }
 
         }
         previous.setOnClickListener {
-            if(i>0){
+            if (i > 0) {
                 i--
                 mapa.setImageResource(images[i])
-            }else{
-                Toast.makeText(applicationContext, "pierwszy obraz", Toast.LENGTH_SHORT ).show()
-                i=0
+            } else {
+                Toast.makeText(applicationContext, "pierwszy obraz", Toast.LENGTH_SHORT).show()
+                i = 0
             }
 
 
         }
         camera.setOnClickListener {
-                var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivityForResult(i, 123)
+            var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(i, 123)
 
 
         }
         delete.setOnClickListener {
             mapa.setImageResource(0)
 
+
+
+
         }
 
+        Suwak.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int,fromUser: Boolean) {
+
+                mapa.setRotation(Suwak.progress.toFloat())
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                Toast.makeText(applicationContext,""+Suwak.progress , Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+
     }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         lateinit var mapa: ImageView
@@ -94,8 +123,11 @@ class MainActivity : AppCompatActivity() {
         if(requestCode==123){
             var bmp = data?.extras?.get("data")as Bitmap
             mapa.setImageBitmap(bmp)
+        } else {
+            Toast.makeText(applicationContext, "nie zrobiono zdjecia", Toast.LENGTH_SHORT).show()
         }
     }
+
 
 
 
